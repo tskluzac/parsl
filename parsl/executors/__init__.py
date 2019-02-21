4070fc77c387e13e94683ad06b602d97d112d6da
tskluzac <skluzacek@uchicago.edu>
2019-02-21 07:27:33 +0100

    Adding simplified version of new executor so Yadu can help me import it into Parsl config.

--- parsl/executors/__init__.py
+++ parsl/executors/__init__.py
@@ -3,9 +3,11 @@ from parsl.executors.ipp import IPyParallelExecutor
 from parsl.executors.high_throughput.executor import HighThroughputExecutor
 from parsl.executors.extreme_scale.executor import ExtremeScaleExecutor
 from parsl.executors.low_latency.executor import LowLatencyExecutor
+from parsl.executors.funcx.executor import FuncXExecutor
 
 __all__ = ['IPyParallelExecutor',
            'ThreadPoolExecutor',
            'HighThroughputExecutor',
            'ExtremeScaleExecutor',
-           'LowLatencyExecutor']
+           'LowLatencyExecutor',
+           'FuncXExecutor']
